d9f4797afe3e8c628a3aec7062e205d53f4b153d
Morten Enemark Lund <melund@gmail.com>
2014-01-08 14:16:15 +0100

    Fix errors in setup.py with new source layout

--- build_installer.bat
+++ build_installer.bat
@@ -1,3 +1,3 @@
-C:\Users\melun_000\Desktop\WinPython-64bit-2.7.5.3\python-2.7.5.amd64\python.exe setup.py sdist
-REM F:\WinPython-64bit\python-2.7.4.amd64\python.exe setup.py bdist_msi
+python.exe setup.py sdist
+REM F:\WinPython-64bit\python-2.7.4.amd64\python.exe src\setup.py bdist_msi
 pause
