9387d45e57a2f516ca4a5ac046cd03a5726fe4e9
Morten Enemark Lund <melund@gmail.com>
2014-03-24 10:05:40 +0100

    Update version number to 0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='AnyPyTools',
-    version='0.4.6',
+    version='0.5',
     install_requires=['scipy', 'numpy','h5py'],
     py_modules=['anypytools.abcutils', 'anypytools.h5py_wrapper',
                 'anypytools.datautils'],
