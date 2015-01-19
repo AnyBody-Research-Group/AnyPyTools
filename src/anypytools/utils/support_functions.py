ebf5fcb98fcc2a8ebca4c83cf34ba7094152b35c
Morten Enemark Lund <melund@gmail.com>
2015-01-19 13:29:52 +0100

    Remove blaze functions from support_functions.py, since it was moved to its own file

--- src/anypytools/utils/support_functions.py
+++ src/anypytools/utils/support_functions.py
@@ -9,12 +9,14 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 try:
     from .py3k import * # @UnusedWildImport
 except (ValueError, SystemError):
-    from py3k import * # @UnusedWildImport
+    from py3k import *  # @UnusedWildImport
 
 import os
 import numpy as np
 import copy
 from collections import OrderedDict
+from ast import literal_eval
+import collections
 
 
 
@@ -26,7 +28,7 @@ def array2anyscript(arr):
             return '{:.12g}'.format(v)
         elif isinstance(v, (string_types, np.str_)):
             return '"{}"'.format(v)
-    
+
     def createsubarr(arr):
         outstr = ""
         if isinstance(arr, np.ndarray):
@@ -46,26 +48,29 @@ def array2anyscript(arr):
     else:
         return str(arr)
 
-def parse_anybodycon_output(strvar):
-    """ Parses the AnyBody console ouput and returns a dictionary with data
-    """
-    out = {};
-    full_variable_name = None
+def _parse_anybodycon_output(strvar):
+    out = OrderedDict();
+    dump_path = None
     for line in strvar.splitlines():
         if line.count('#### Macro command') and line.count('"Dump"'):
             me = re.search('Main[^ \"]*', line)
             if me is not None :
-                full_variable_name = me.group(0)
+                dump_path = me.group(0)
         if line.endswith(';') and line.count('=') == 1:
-            (var_name, var_data) = line.split('=')
-            var_name = var_name.strip()
-            var_data = var_data.strip(' ;').replace('{','[').replace('}',']')
-            if full_variable_name is not None:
-                var_name = full_variable_name
-                full_variable_name = None
-            out[var_name.strip()] = np.array(eval(var_data))
-        
-        if line.startswith('ERROR') or line.startswith('Error'): 
+            (first, last) = line.split('=')
+            first = first.strip()
+            last = last.strip(' ;').replace('{','[').replace('}',']')
+            if dump_path is not None:
+                first = dump_path
+                dump_path = None
+            try:
+                out[first.strip()] = np.array(literal_eval(last))
+            except SyntaxError:
+                pass
+
+        if ( line.startswith('ERROR') or
+             line.startswith('Error') or 
+             line.startswith('Model loading skipped')) : 
             if 'ERROR' not in out:
                 out['ERROR'] = []
             out['ERROR'].append(line)
@@ -100,86 +105,9 @@ def _run_from_ipython():
     except NameError:
         return False
         
-def _get_datashape(result_list):
-    try: 
-        from datashape import discover
-        from datashape.coretypes import (Record, var,Option,
-                                        int64,string, float64, Fixed)
-    except ImportError:
-        import warnings
-        warnings.warn('blaze and datashape packages must be installed,' 
-                      ' to convert data to the blaze format.',
-                      ImportWarning)
-        raise ImportWarning
 
-    fields = OrderedDict()
-    for result in result_list:
-        for key in result.keys():
-            if key not in fields:
-                dshape = discover(result[key])
-                if len(dshape) > 1:
-                    dshape = var * dshape.subarray(1) 
-                else:
-                    dshape = dshape
-                fields[key] = dshape
-    
-    def update_fields(key, ds):
-        if key in fields:
-            del fields[key]
-            fields[key] = ds
-    
-    update_fields('ERROR', var*string )
-    update_fields('task_macro_hash',  Fixed(1) *int64 ) 
-    update_fields('task_id',   Fixed(1) *int64 )
-    update_fields('task_work_dir',  Fixed(1) *string )
-    update_fields('task_name',  Fixed(1) *string )
-    update_fields('task_processtime', Fixed(1) *float64 )
-    update_fields('task_macro', var*string ) 
-    update_fields('task_logfile', Fixed(1) *string ) 
-    
-    if not fields:
-        return Fixed(len(result_list)) * Option(float64)
-    else:
-        return ( Fixed(len(result_list)) * 
-                 Record( tuple( (k, v) for k, v in fields.items()) ) )
         
-def convert_to_blaze_data(result_list):
-    from blaze import Data
-    
-    # Remove all '.' in keys
-    for result in result_list:
-        for key in result.keys():
-            if isinstance(result[key], np.ndarray):
-                result[key] = result.pop(key).tolist()
-            if '.' in key:
-                result[key.replace('.','_')] = result.pop(key)
-                
-    datashape = _get_datashape( result_list ) 
-
-    # Create a set of all keys in all results
-    fields_set = set()
-    for result in result_list:
-        fields_set.update( result.keys() ) 
-    # Ensure that these keys are present in all results.
-    for result in result_list:
-        for field in fields_set:
-            if field not in result:
-                result[field] = []
-    # If there are no keys, just make the result a list of 
-    # None instead of list empty dicts
-    
-    if not fields_set:
-        return None
-    
-    return  Data(result_list, dshape = datashape)  
-    
-    
-    
-    
-    
-    
-        
-        
+ 
         
 def make_hash(o):
 
@@ -192,7 +120,7 @@ def make_hash(o):
 
   if isinstance(o, (set, tuple, list)):
 
-    return tuple([make_hash(e) for e in o])    
+    return hash( tuple([make_hash(e) for e in o]) )  
 
   elif not isinstance(o, dict):
 
