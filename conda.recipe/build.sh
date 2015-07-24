a6886142b62aa13c87f85695e04accf440011d7b
Morten Enemark Lund <melund@gmail.com>
2015-07-24 22:04:54 +0200

    Removed the workaround for launching the tutorial...

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 $PYTHON setup.py install
+
+cd ${PREFIX}
+mkdir AnyPyToolsTutorial
+cp -R ${SRC_DIR}/Tutorial/* ${PREFIX}/AnyPyToolsTutorial/
+#cp ${RECIPE_DIR}/AnyPyToolsTutorial.bat ${SCRIPTS}/AnyPyToolsTutorial.bat
+#cp ${RECIPE_DIR}/AnyPyToolsTutorial.bat ${PREFIX}/bin/AnyPyToolsTutorial.bat
