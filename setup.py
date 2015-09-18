3459282e468e267a2458c506c29f17ad46f9389d
Morten Enemark Lund <melund@gmail.com>
2015-09-18 08:17:50 +0200

    Fix wrong name for pygments_plugin package

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
                 'anypytools.utils.py3k', 'anypytools.utils.pytest_plugin',
                 'anypytools.utils.support_functions',
                 'anypytools.utils.blaze_converter',
-                'anypytools.anyscript_pygments.anyscript_lexer',
-                'anypytools.anyscript_pygments.anyscript_style'],
+                'anypytools.pygments_plugin.anyscript_lexer',
+                'anypytools.pygments_plugin.anyscript_style'],
     scripts=['src/scripts/pp2any.py'],
     packages=['anypytools'],
     package_dir={'': 'src'},
@@ -29,11 +29,11 @@ setup(
             'anypytools = anypytools.utils.pytest_plugin',
         ],
         'pygments.lexers': 
-            ['anyscript = anypytools.anyscript_pygments.anyscript_lexer:AnyScriptLexer',
-            '/.any = anypytools.anyscript_pygments.anyscript_lexer:AnyScriptLexer'],
+            ['anyscript = anypytools.pygments_plugin.anyscript_lexer:AnyScriptLexer',
+            '/.any = anypytools.pygments_plugin.anyscript_lexer:AnyScriptLexer'],
         'pygments.styles': 
-            ['anyscript = anypytools.anyscript_pygments.anyscript_style:AnyScriptStyle',
-             '/.any = anypytools.anyscript_pygments.anyscript_style:AnyScriptStyle' ]
+            ['anyscript = anypytools.pygments_plugin.anyscript_style:AnyScriptStyle',
+             '/.any = anypytools.pygments_plugin.anyscript_style:AnyScriptStyle' ]
     },
     author='Morten Lund',
     author_email='melund@gmail.com',
