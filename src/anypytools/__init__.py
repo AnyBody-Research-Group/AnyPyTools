7740bfa199858a597ec7574c02dcd2fafbd528a8
Morten Enemark Lund <melund@gmail.com>
2016-11-18 11:03:36 +0100

    Update version to 0.9.3

--- src/anypytools/__init__.py
+++ src/anypytools/__init__.py
@@ -14,7 +14,7 @@ __all__ = ['datautils', 'h5py_wrapper', 'AnyPyProcess',
            'MacroGenerator', 'MonteCarloMacroGenerator', 'LatinHyperCubeMacroGenerator'
            'AnyMacro', 'macro_commands', 'print_versions']
 
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 
 
 def print_versions():
