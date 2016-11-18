b19ee55ee675e8cf8d86963d01f273d9af99165b
Morten Enemark Lund <melund@gmail.com>
2016-11-18 11:03:20 +0100

    Remove the conda-recipe, as this is now on conda-forge

--- conda.recipe/AnyPyToolsTutorial.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd %~f0\..\..\share\AnyPyToolsTutorial
-"%~f0\..\jupyter.exe" "notebook" "--notebook-dir=." "00_AnyPyTools_tutorial.ipynb" %*
--- conda.recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-$PYTHON setup.py install
-
-cd ${PREFIX}
-mkdir AnyPyToolsTutorial
-cp -R ${SRC_DIR}/Tutorial/* ${PREFIX}/AnyPyToolsTutorial/
-#cp ${RECIPE_DIR}/AnyPyToolsTutorial.bat ${SCRIPTS}/AnyPyToolsTutorial.bat
-#cp ${RECIPE_DIR}/AnyPyToolsTutorial.bat ${PREFIX}/bin/AnyPyToolsTutorial.bat
