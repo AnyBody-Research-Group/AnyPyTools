eac8b3edc54ddad884213bb461aba04dcc7d2d00
Morten Enemark Lund <melund@gmail.com>
2017-06-29 12:41:11 +0200

    Style fixes

--- src/anypytools/pygments_plugin/anyscript_style.py
+++ src/anypytools/pygments_plugin/anyscript_style.py
@@ -14,40 +14,40 @@ from pygments.token import Keyword, Name, Comment, String, Error, Number, Operat
 __all__ = ['AnyScriptStyle']
 
 class AnyScriptStyle(Style):
-    background_color="#f8f8f8"
+    background_color = "#f8f8f8"
     default_style = ""
 
     styles={
-          Whitespace:                "#bbbbbb", 
-          Comment:                   "noitalic #4AA02C", 
-          Comment.Preproc:           "noitalic #0000FF", 
-   
-          #Keyword:                   "bold #AA22FF", 
-          Keyword:                   "#0000FF", 
-          Keyword.Pseudo:            "nobold", 
-          Keyword.Type:              "nobold #0000FF", 
-   
+          Whitespace:                "#bbbbbb",
+          Comment:                   "noitalic #4AA02C",
+          Comment.Preproc:           "noitalic #0000FF",
+
+          #Keyword:                   "bold #AA22FF",
+          Keyword:                   "#0000FF",
+          Keyword.Pseudo:            "nobold",
+          Keyword.Type:              "nobold #0000FF",
+
           Operator:                  "#111111", 
-          Operator.Word:             "bold #AA22FF", 
-   
-          Name.Builtin:              "#0000FF", 
-          Name.Function:             "#0000FF", 
-          Name.Class:                "#0000FF", 
-          Name.Namespace:            "#900090", 
-          Name.Exception:            "#D2413A", 
-          Name.Variable:             "#19177C", 
-          Name.Constant:             "#880000", 
-          Name.Label:                "#A0A000", 
-          Name.Entity:               "bold #999999", 
-          Name.Attribute:            "#7D9029", 
-          Name.Tag:                  "bold #008000", 
-          Name.Decorator:            "#AA22FF", 
-	
-		  Other.Statements:			 "#900090",
-		  Other.Options:			 "bold",
-   
-          String:                    "#666666", 
-          String.Doc:                "italic", 
+          Operator.Word:             "bold #AA22FF",
+
+          Name.Builtin:              "#0000FF",
+          Name.Function:             "#0000FF",
+          Name.Class:                "#0000FF",
+          Name.Namespace:            "#900090",
+          Name.Exception:            "#D2413A",
+          Name.Variable:             "#19177C",
+          Name.Constant:             "#880000",
+          Name.Label:                "#A0A000",
+          Name.Entity:               "bold #999999",
+          Name.Attribute:            "#7D9029",
+          Name.Tag:                  "bold #008000",
+          Name.Decorator:            "#AA22FF",
+
+          Other.Statements:          "#900090",
+          Other.Options:             "bold",
+
+          String:                    "#666666",
+          String.Doc:                "italic",
           String.Interpol:           "bold #BB6688", 
           String.Escape:             "bold #BB6622", 
           String.Regex:              "#BB6688", 
