d644ff5a51f59fc47ac1b999e851ff4218f382e9
Morten Enemark Lund <melund@gmail.com>
2018-02-14 13:23:11 +0100

    pep8

--- anypytools/__init__.py
+++ anypytools/__init__.py
@@ -9,7 +9,7 @@ from anypytools.abcutils import AnyPyProcess, execute_anybodycon
 from anypytools.macroutils import AnyMacro
 from anypytools import macro_commands
 from anypytools.tools import (ABOVE_NORMAL_PRIORITY_CLASS, BELOW_NORMAL_PRIORITY_CLASS,
-                        IDLE_PRIORITY_CLASS, NORMAL_PRIORITY_CLASS)
+                              IDLE_PRIORITY_CLASS, NORMAL_PRIORITY_CLASS)
 
 
 logger = logging.getLogger('abt.anypytools')
